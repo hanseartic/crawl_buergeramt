7a7a085a4dbbdd2cf5ed3f808bf7b8de5eaedd1a
Paul Schütte <paul.schuette@mailbox.org>
2015-11-06 16:07:39 +0100

    Generalizes crawler

--- Crawlers.py
+++ Crawlers.py
@@ -1,54 +1,191 @@
 import logging
-from queue import Queue
+from queue import Queue, Empty
 from urllib.parse import urlparse, parse_qs
-
 import requests
 import lxml.html as lhtml
 import threading
-
 import time
 from requests.exceptions import ReadTimeout
-
 from ProxyManager import BaseProxyManager
+from pydispatch import dispatcher
+
+
+class TooManyRequestsError(ConnectionRefusedError):
+    pass
 
 
 class Crawler(object):
-    BASE_URL = u'https://service.berlin.de/terminvereinbarung/termin/'
 
-    def __init__(self, proxy_manager: BaseProxyManager = BaseProxyManager()):
-        self.queue = Queue()
-        self.proxy_manager = proxy_manager
-        self.user_agent = 'BuergeramtTermine'
+    SIGNAL_PROGRESS = 'crawler.progress'
+    SIGNAL_TOO_MANY_REQUESTS = 'crawler.too_many_requests'
+    SIGNAL_TIMEOUT = 'crawler.connection_timeout'
+    SIGNAL_TERMINATE = 'crawler.terminate'
+    SIGNAL_TERMINATED = 'crawler.terminated'
+
+    def __init__(self, worker_count=1, name='Crawler', worker_callback: callable = None):
+        dispatcher.connect(self._abort, signal=self.SIGNAL_TERMINATE)
+
+        self.params = {}
+        self.headers = {'User-Agent': 'PyPoeci'}
+        self.css_selector = 'a'  # find all links - most probably too general
+        self.connection_timeout = 30
+
         self.lock = threading.RLock()
+        self.queue = Queue()
+        self.http_session = requests.Session()
+        # share the http_session if the callback is a crawler-instance
+        # this will probably decrease the number of refused connections
+        if isinstance(worker_callback, Crawler):
+            worker_callback.http_session = self.http_session
 
-    def set_user_agent(self, user_agent):
-        self.user_agent = user_agent
+        self.worker_callback = worker_callback
+        self.worker_count = worker_count
+        self.name = name
 
-    def get_headers(self):
-        return {
-            'User-Agent': self.user_agent
-        }
+        self.start_workers(worker_count=worker_count, worker=self.parse, worker_args=[worker_callback])
 
-    workers = [threading.Thread]
+    def __del__(self):
+        self.abort_workers()
 
-    def end_workers(self):
+    def set_timeout(self, timeout):
+        self.connection_timeout = timeout
+
+    def add_header(self, key, value):
+        self.headers[key] = value
+
+    def add_param(self, key, value):
+        if isinstance(value, list):
+            key += '[]'
+        self.params[key] = value
+
+    def set_selector(self, css_selector):
+        self.css_selector = css_selector
+
+    def _abort(self):
+        logging.debug('Received TERMINATE signal. Preparing shutdown.')
+        self.abort_workers()
+
+    workers = []
+
+    def abort_workers(self):
+        try:
+            while True:
+                # empty the queue
+                logging.debug('Clearing worker queue')
+                self.queue.get_nowait()
+                self.queue.task_done()
+        except Empty:
+            pass
+        finally:
+            # signal workers to stop
+            logging.debug('Worker queue is empty')
+            self.stop_workers()
+
+    def stop_workers(self):
         for i in range(len(self.workers)):
-            logging.debug('Sending worker {} stop-signal'.format(i))
+            logging.debug('Sending stop-signal to worker [{}]'.format(i))
             self.queue.put(None)
         for worker in self.workers:
             worker.join()
+            logging.debug('Worker [{}] stopped.'.format(worker.name))
+        self.workers.clear()
         return True
 
-    def start_workers(self, worker_count, target):
+    def start_workers(self, worker_count: int, worker: callable, worker_args=None):
         for i in range(worker_count):
-            logging.debug('Starting worker {}'.format(i))
-            t = threading.Thread(target=target)
+            worker_name = 'Worker {}'.format(i)
+            logging.debug('Starting worker [{}]'.format(worker_name))
+            t = threading.Thread(target=worker, args=worker_args, name=worker_name)
             t.start()
             self.workers.append(t)
 
+    def parse(self, callback: callable):
+        while True:
+            try:
+                link = self.queue.get()
+                if link is None:
+                    break
+
+                link_target = link.attrib['href']
+                with self.lock:
+                    logging.debug('{}'.format(link_target))
+                if hasattr(callback, 'crawl'):
+                    callback.crawl(link_target)
+                else:
+                    callback(link_target)
+            finally:
+                self.queue.task_done()
+
+    def crawl(self, url):
+        crawl_thread = threading.Thread(target=self._main_crawler, args=[url])
+        crawl_thread.start()
+        return crawl_thread
+
+    def _main_crawler(self, url):
+        logging.debug('{} started'.format(self.name))
+        # do the crawl and
+        # queue the extracted targets
+        with self.lock:
+            try:
+                response = self.http_session.get(
+                    url=url,
+                    params=self.params,
+                    headers=self.headers,
+                    timeout=self.connection_timeout,
+                )
+                if response.status_code == 429:
+                    raise TooManyRequestsError()
+                self._notify_progress()
+                html = response.text
+                tree = lhtml.fromstring(html)
+                links_to_follow = tree.cssselect(self.css_selector)
+                # "td[class~='{}']>a".format(self.CSS_CLASS_RESERVABLE))
+                if len(links_to_follow) > 0:
+                    logging.info('Found {} links. Queueing for work'.format(len(links_to_follow)))
+                    for link in links_to_follow:
+                        self.queue.put(link)
+            except ReadTimeout as rt:
+                logging.info(
+                    'Connection attempt to {} timed out after {} seconds.'.format(
+                        urlparse(rt.request.url).path,
+                        self.connection_timeout
+                    )
+                )
+                self._notify_timeout(url)
+            except TooManyRequestsError:
+                logging.debug('Too many requests made')
+                self._notify_too_many_requests(url)
+            except Exception as e:
+                logging.warning('Could not parse resulting page. {}'.format(e))
+            finally:
+                self.queue.join()
+        logging.debug('{} done'.format(self.name))
+
+    def _notify_progress(self):
+        with self.lock:
+            dispatcher.send(signal=self.SIGNAL_PROGRESS, sender=self)
+
+    def _notify_too_many_requests(self, requested_url):
+        with self.lock:
+            dispatcher.send(
+                sender=self,
+                signal=self.SIGNAL_TOO_MANY_REQUESTS,
+                url=requested_url,
+            )
+
+    def _notify_timeout(self, requested_url):
+        with self.lock:
+            dispatcher.send(
+                sender=self,
+                signal=self.SIGNAL_TIMEOUT,
+                url=requested_url,
+            )
+
 
 class CalendarCrawler(Crawler):
-    URL_CALENDAR = Crawler.BASE_URL + u'tag.php'
+
+    BASE_URL = u'https://service.berlin.de/terminvereinbarung/termin/'
+    URL_CALENDAR = BASE_URL + u'tag.php'
     CSS_CLASS_RESERVABLE = 'buchbar'
 
     param_service_ids = [  # dienstleister
@@ -109,24 +246,18 @@ class CalendarCrawler(Crawler):
                     )
                     self.proxy_manager.renew_connection()
                 except Exception as e:
-                    logging.warning('Could not parse resulting page.')
-            # wait for links to be crawled
+                    logging.warning('Could not parse resulting page. {}'.format(e))
+                    # wait for links to be crawled
         self.queue.join()
 
         # return home
-        if self.end_workers():
+        if self.stop_workers():
             print('>', end='', flush=True)
 
     def set_start_date(self, start_date):
         self.start_date = start_date
 
-    def get_params(self):
-        return {
-            'termin': 1,
-            'Datum': self.start_date,
-            'dienstleister[]': self.param_service_ids,
-            'anliegen[]': self.param_request,
-        }
+
 
     def handle_calendar_links(self, calendar_links):
         logging.info('Found {} links. Queueing for work'.format(len(calendar_links)))
@@ -149,7 +280,8 @@ class CalendarCrawler(Crawler):
 
 
 class DetailCrawler(Crawler):
-    URL_DAY = Crawler.BASE_URL + u'termin.php'
+    BASE_URL = u'https://service.berlin.de/terminvereinbarung/termin/'
+    URL_DAY = BASE_URL + u'termin.php'
 
     def select_appointment(self):
         while True:
@@ -158,7 +290,7 @@ class DetailCrawler(Crawler):
                 break
             with self.lock:
                 logging.debug('Reserving {}'.format(item))
-            #  fetch appointment_page
+            # fetch appointment_page
             #  extract form data
             #  fill form
             #  make post request
@@ -188,4 +320,4 @@ class DetailCrawler(Crawler):
                 break
 
         self.queue.join()
-        self.end_workers()
+        self.stop_workers()
