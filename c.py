146c74f78ea35c79e0381b7625a7f4f6d3622a14
Paul Schütte <paul.schuette@mailbox.org>
2015-11-11 23:03:08 +0100

    Adds crawler for forms and uses local DB

--- c.py
+++ c.py
@@ -3,11 +3,12 @@ import sys
 import argparse
 import time
 import logging
-from threading import Thread
-
+import lxml.html as lhtml
+import sqlite3
 import json_log_filter
+import db
 from Crawlers import Crawler
-from ProxyManager import TorProxyManager, BaseProxyManager
+from ProxyManager import TorProxyManager
 from pydispatch import dispatcher
 
 CSS_CLASS_RESERVABLE = 'buchbar'
@@ -50,6 +51,8 @@ def get_date(start_date):
 
 
 pm = None
+sqlite_connection = sqlite3.connect('buergeramt.db')
+db_cursor = sqlite_connection.cursor()
 
 
 def main(args):
@@ -64,6 +67,8 @@ def main(args):
 
     #  don't show the TRACEs from stem in the logs
     logging.getLogger('stem').addFilter(lambda rec: rec.levelname.upper() != 'TRACE')
+
+    db.seed(db_cursor)
 
     logging.info('Start searching for free appointments')
     arguments = parse_args(args)
@@ -81,13 +86,13 @@ def main(args):
     dispatcher.connect(receiver=on_crawler_match, signal=Crawler.SIGNAL_OUT_MATCH_FOUND)
 
     form_crawler = Crawler(worker_count=1, name='Form Filter')
-    form_crawler\
-        .set_timeout(5)\
-        .set_selector('#kundendaten form')\
+    form_crawler \
+        .set_timeout(5) \
+        .set_selector('#kundendaten form') \
         .add_header(
-            'User-Agent',
-            'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
-        )
+        'User-Agent',
+        'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
+    )
 
     details_crawler = Crawler(worker_count=8, name='Details Crawler', worker_callback=form_crawler)
     details_crawler.set_timeout(15)
@@ -98,7 +103,7 @@ def main(args):
         'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
     )
 
-    #calendar_crawler = Crawler(worker_count=4, name='Calendar crawler', worker_callback=calendar_callback)
+    # calendar_crawler = Crawler(worker_count=4, name='Calendar crawler', worker_callback=calendar_callback)
     calendar_crawler = Crawler(worker_count=4, name='Calendar crawler', worker_callback=details_crawler)
     calendar_crawler.add_header(
         'User-Agent',
@@ -116,6 +121,7 @@ def main(args):
     while run:
         ct = calendar_crawler.crawl(URL_CALENDAR)
         ct.join()
+        time.sleep(20)
 
     for thread in crawl_threads:
         thread.join()
@@ -162,24 +168,31 @@ def on_crawler_match(sender: Crawler, match, source_url):
         pass
     elif sender.name == 'Details crawler':
         pass
-    else:
-        pass
-
-
-def calendar_callback(detail_link):
-    with Crawler(worker_count=10, name='Details Crawler',
-                 worker_callback=reserve_appointment) as details_crawler:
-
-        logging.debug('callback received detail-link')
-        details_crawler.set_timeout(15)
-        details_crawler.set_selector("td[class~='{}']>a".format(CSS_CLASS_FREE_APPOINTMENT))
-        details_crawler.add_header(
-            'User-Agent',
-            'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
+    elif sender.name == 'Form Filter':
+        db_cursor.execute(
+            "SELECT id, name, phone, mail FROM `customers` where `appointment` = '' ORDER BY `updated` LIMIT 1"
         )
-        details_crawler.crawl(
-            BASE_URL + detail_link
-        ).join()
+        customer = db_cursor.fetchone()
+        if customer:
+            form = match
+            form.inputs['Nachname'].value = customer[1]   # Joern Kamphuis
+            form.inputs['EMail'].value = customer[3]      # 'joern@dingbone.com'
+            if 'telefonnummer_fuer_rueckfragen' in form.inputs.keys():
+                form.inputs['telefonnummer_fuer_rueckfragen'].value = customer[2]  # '05948933496'
+            if 'telefon' in form.inputs.keys():
+                form.inputs['telefon'].value = customer[2]
+            form.inputs['agbgelesen'].checked = True
+            result = lhtml.parse(lhtml.submit_form(form)).getroot()
+            cancel_tokens = result.cssselect('.number-red-big')
+            cancel_token = 0
+            if len(cancel_tokens) > 0:
+                try:
+                    cancel_token = cancel_tokens[0].text
+                except Exception:
+                    pass
+            db_cursor.execute("UPDATE `customers` SET appointment=1, cancel_token=?, confirmation=?", (cancel_token, result))
+            logging.debug('got appointment for {}'.format(customer[1]))
+        pass
 
 
 def reserve_appointment(appointment_link):
@@ -189,8 +202,10 @@ def reserve_appointment(appointment_link):
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Find free appointments on buergeramt website.')
     parser.add_argument('--start_date', '-d', help='define the start date')
+    parser.add_argument('--log-level', '-l', help='What should be logged',
+                        choices=['DEBUG', 'INFO', 'WARN', 'ERROR'], default='INFO')
     parser.add_argument('--tor', '-t', help='If you want to use tor', action='store_true')
-    # parser.add_argument('--socks', '-s', help='Use a socks5 proxy', nargs='+')
+    # parser.add_argument('--socks', '-s', help='Use a socks5 proxy')
     arguments = parser.parse_args(args)
     return arguments
 
---
+++ db.py
@@ -0,0 +1,57 @@
+from sqlite3 import Cursor
+
+
+service_seed = [  # anliegen
+    (120703, 'Personalausweis beantragen'),
+    (120686, 'Anmelden einer Wohnung'),
+    (121151, 'Reisepass beantragen'),
+    (121469, 'Kinderreisepass beantragen'),
+    (120926, 'Führungszeugnis beantragen'),
+    (120702, 'Meldebescheinigung beantragen'),
+    (121627, 'Ersterteilung Führerschein'),
+    (121629, 'Erweiterung Führerschein'),
+    (121637, 'Neuerteilung Führerschein nach Entzug'),
+    (121593, 'Ersatzführerschein nach Verlust'),
+]
+
+location_seed = [  # dienstleister
+    (122210, ''), (122217, ''), (122219, ''), (122227, ''), (122231, ''), (122238, ''), (122243, ''), (122252, ''),
+    (122260, ''), (122262, ''), (122254, ''), (122271, ''), (122273, ''), (122277, ''), (122280, ''), (122282, ''),
+    (122284, ''), (122291, ''), (122285, ''), (122286, ''), (122296, ''), (150230, ''), (122301, ''), (122297, ''),
+    (122294, ''), (122312, ''), (122314, ''), (122304, ''), (122311, ''), (122309, ''), (317869, ''), (324433, ''),
+    (325341, ''), (324434, ''), (324435, ''), (122281, ''), (324414, ''), (122283, ''), (122279, ''), (122276, ''),
+    (122274, ''), (122267, ''), (122246, ''), (122251, ''), (122257, ''), (122208, ''), (122226, ''),
+]
+
+
+def seed(db: Cursor):
+    db.execute('''CREATE TABLE IF NOT EXISTS `customers` (
+    id int primary key,
+    name text,
+    phone text,
+    mail text,
+    updated int,
+    cancel_token text,
+    appointment text,
+    confirmation blob
+)''')
+    db.execute("SELECT `name` FROM 'sqlite_master' WHERE type='table' AND name='services'")
+    if not db.fetchone():
+        db.execute('''CREATE TABLE IF NOT EXISTS `services` (
+    id int(8) primary key,
+    name text
+)''')
+        db.executemany("INSERT INTO `services` VALUES(?,?)", service_seed)
+    db.execute("SELECT `name` FROM 'sqlite_master' WHERE type='table' AND name='locations'")
+    if not db.fetchone():
+        db.execute('''CREATE TABLE IF NOT EXISTS `locations` (
+    id int(8) primary key,
+    name text
+)''')
+        db.executemany("INSERT INTO `locations` VALUES(?,?)", location_seed)
+    db.connection.commit()
+
+    db.execute('select * from services limit 2')
+    v = db.fetchall()
+    d = db.description
+    pass
